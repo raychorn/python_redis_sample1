371eb6253cfb0d837e2a983489537c28fb913fe9
Ray C Horn <raychorn@gmail.com>
2021-02-12 11:30:02 -0700

    updates.

--- README.md
+++ README.md
@@ -37,4 +37,10 @@ redis-cli info stats
 redis-cli info server
 ```
 
+### RQ (Redis Queue) is a simple Python library for queueing jobs and processing them in the background with workers. It is backed by Redis and it is designed to have a low barrier to entry. It can be integrated in your web stack easily.
+
+```
+https://python-rq.org/
+```
+
 (c). Copyright 2021, Ray C Horn, All Rights Reserved.
--- redis-prototype1.py
+++ redis-prototype1.py
@@ -1,7 +1,26 @@
 import redis
+import datetime
+
+def count_words_at_url(url):
+    import requests
+    resp = requests.get(url)
+    return len(resp.text.split())
+
+
+def say_hello():
+    print('Saying Hello.')
+    
 
 if (__name__ == '__main__'):
-    r = redis.Redis(host='localhost', port=6379, db=0)
-    r.set('foo', 'bar')
+    if (0):
+        r = redis.Redis(host='localhost', port=6379, db=0)
+        r.set('foo', 'bar')
 
-    print(r.get('foo'))
+        print(r.get('foo'))
+        
+    if (1):
+        from rq import Queue
+        q = Queue(connection=redis.Redis())
+        result = q.enqueue(count_words_at_url, 'http://nvie.com')
+        
+        job = q.enqueue_in(datetime.timedelta(seconds=10), say_hello)
