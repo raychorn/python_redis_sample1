89f5a8e0a204e6f504b8f02cf980d5b0b2c1911a
Ray C Horn <raychorn@gmail.com>
2021-02-15 12:09:20 -0700

    updates.

--- redis-pub-sub1.py
+++ redis-pub-sub1.py
@@ -12,20 +12,30 @@ class RedisPub():
         
 
 class RedisSub():
-    def __init__(self, name, host='127.0.0.1', port=6379, db=0):
+    def __init__(self, name, host='127.0.0.1', port=6379, db=0, callback=None):
         self.queue = redis.StrictRedis(host=host, port=port, db=db)
         self.channel = self.queue.pubsub()
+        self.callback = callback
 
         self.channel.subscribe(name)
 
     def sub(self):
         message = None
+        cnt = 0
         while (1):
             message = self.channel.get_message()
+            print('message -> {}'.format(message))
             if message and (not message['data'] == 1):
                 message = message['data'].decode('utf-8')
                 break
-            time.sleep(0.1)
+            cnt += 1
+            if (cnt % 10) == 0:
+                if (callable(self.callback)):
+                    try:
+                        self.callback(self, cnt)
+                    except:
+                        pass
+                time.sleep(0.1)
         return message
         
 
@@ -33,9 +43,12 @@ if (__name__ == '__main__'):
     name = 'chan1'
     value1 = 'Test1'
     s = RedisPub()
-    s.pub(name, value1)
+
+    def callback(rp, cnt):
+        print('Callback!')
+        s.pub(name, value1)
     
-    p = RedisSub(name)
+    p = RedisSub(name, callback=callback)
     m = p.sub()
     assert m == value1, 'Oops, something wrong because we expected {} but got {}.'.format(value1, m)
     print('Got {} which was expected.'.format(m))
